ee12e11755774de850acb5f184d42c8cabfff9c9
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2025-07-20 17:44:19 +0800

    Add tuna mirror to fasten downloads
    Signed-off-by: Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>

--- common/modules/system.nix
+++ common/modules/system.nix
@@ -54,16 +54,17 @@
       "flakes"
     ];
     substituters = [
-      "https://cache.nixos.org" # Official cache (default)
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store/" # Tsinghua University mirror
       "https://nix-community.cachix.org" # Nix community cache
       "https://cache.garnix.io" # Garnix cache
+      "https://cache.nixos.org" # Official cache (default)
     ];
 
     # Trusted public keys for the caches
     trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbCHOoEo8tQdAq3n8l0="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
 
     # Build settings
