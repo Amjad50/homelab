f2a5a8a9d1a8b0feabfde5113ed86dbac407cfd7
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2025-08-13 20:11:46 +0800

    Update ipv6 config for middle machine
    Signed-off-by: Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>

--- machines/middle/networking.nix
+++ machines/middle/networking.nix
@@ -2,12 +2,20 @@
 {
   networking.hostName = "middle";
 
-  networking.interfaces.ens3.ipv6.addresses = [
-    {
-      address = "2a03:4000:5:1ab::abcd";
-      prefixLength = 64;
-    }
-  ];
+  systemd.network.links."10-net-main" = {
+    matchConfig.Path = "pci-0000:00:12.0";
+    linkConfig.Name = "ens18";
+  };
+  networking.interfaces.ens18 = {
+    useDHCP = true; 
+    ipv6.addresses = [
+      { address = "2407:3640:2270:5255::1"; prefixLength = 64; }
+    ];
+  };
+  networking.defaultGateway6 = {
+    address = "fe80::1";   # common link-local gw in VPS hosts
+    interface = "ens18";   # must specify interface for fe80::
+  };
 
   # IPv6 NAT support for WireGuard
   boot.kernelModules = [ "ip6table_nat" ];
