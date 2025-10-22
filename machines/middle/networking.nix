c0fcf5b19d6fc2baef7123f8089a4d193105444e
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2025-10-23 07:20:54 +0800

    use static ip for middle machine
    Signed-off-by: Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>

--- machines/middle/networking.nix
+++ machines/middle/networking.nix
@@ -3,18 +3,27 @@
   networking.hostName = "middle";
 
   networking.interfaces.ens18 = {
-    useDHCP = true;
-
+    useDHCP = false;
     ipv6.addresses = [
       {
         address = "2407:3640:2270:5255::1";
         prefixLength = 64;
       }
     ];
+    ipv4.addresses = [
+      {
+        address = "62.146.239.217";
+        prefixLength = 21;
+      }
+    ];
   };
   networking.defaultGateway6 = {
     address = "fe80::1"; # common link-local gw in VPS hosts
     interface = "ens18"; # must specify interface for fe80::
+  };
+  networking.defaultGateway = {
+    address = "62.146.232.1";
+    interface = "ens18";
   };
 
   # IPv6 NAT support for WireGuard
