452351f3fbc10ee23d3020bdeec3c158c41be2b9
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2026-05-08 11:58:30 +0800

    changed router, new address for home

--- machines/home/networking.nix
+++ machines/home/networking.nix
@@ -14,13 +14,13 @@
     useDHCP = false;
     ipv4.addresses = [
       {
-        address = "192.168.0.5";
+        address = "192.168.100.5";
         prefixLength = 24;
       }
     ];
   };
   networking.defaultGateway = {
-    address = "192.168.0.1";
+    address = "192.168.100.1";
     interface = "eno2";
   };
 }
